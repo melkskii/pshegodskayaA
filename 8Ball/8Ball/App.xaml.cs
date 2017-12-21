bb8fb67e0ef58301d3843694365cabfceb99f4bf
Алина Пшегодская <poloska666@yandex.ru>
2017-12-21 03:40:57 +0300

    обр ошибок

--- 8Ball/8Ball/App.xaml.cs
+++ 8Ball/8Ball/App.xaml.cs
@@ -11,7 +11,17 @@ namespace _8Ball
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
+    /// 
+
+
     public partial class App : Application
     {
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Exception Simple", MessageBoxButton.OK, MessageBoxImage.Warning);
+            e.Handled=true;
+        }
+
+
     }
 }
